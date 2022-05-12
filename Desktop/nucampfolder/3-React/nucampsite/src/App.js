70bdca4cf8401f316b889e926d14ea2e622268e2
Samantha Goulding <srgoulding21@gmail.com>
2022-05-12 03:13:41 -0400

    useState

---
+++ Desktop/nucampfolder/3-React/nucampsite/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesDirectoryPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/nucampfolder/3-React/nucampsite/src/components/Header.js
@@ -0,0 +1,17 @@
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "../app/assets/img/logo.png";
+// React from "@types/react";
+
+const Header = () => {
+  return (
+    <Navbar dark color="primary" sticky="top" expand="md">
+      <Container>
+        <NavbarBrand href="/">
+          <img src={NucampLogo} alt="nucamp logo" />
+        </NavbarBrand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
